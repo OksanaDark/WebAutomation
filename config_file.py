570d8dfb9695d58841a4198a749e6a6ef1c93028
Oksana Lu <thedarkstaf@gmail.com>
2023-03-17 16:07:59 +0200

    some shit

---
+++ config_file.py
@@ -0,0 +1,12 @@
+site_url = "http://192.168.0.15:8080/sample-page/"
+
+admin_user = "Admin"
+admin_pass = "9(U1HK(X$(%xHXkthz54NXea"
+
+subscriber_user = "Subscriber"
+subscriber_pass = "tI2DPbUgQta**^npq5xi932g"
+subscriber_email = "subscriber@somemail.com"
+
+
+
+
